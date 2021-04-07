bf10e1f74961abbafa8824a6d398f89f27713ea5
tubaman48 <rjlovett48@gmail.com>
2021-04-07 19:02:23 +0000

    add js toggle function from popup markers for favourites

--- assets/js/mymap.js
+++ assets/js/mymap.js
@@ -1,16 +1,24 @@
-let mapTileLayers = L.tileLayer("http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
+// apply map
+
+var mapTileLayers = L.tileLayer("http://services.arcgisonline.com/arcgis/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}", {
     attribution: "Powered by <a href='https://developers.arcgis.com/terms/attribution/' target='_blank' rel='noopener'>Esri</a>"
 });
 
-let mymap = L.map("mapid", {
+var mymap = L.map("mapid", {
     layers: [mapTileLayers],
     center: [51.27, 0.25], // centred on Tunbridge Wells
-    zoom: 7
+    zoom: 8,
+    maxBounds: [
+        // stops map from infinite scrolling at edges
+        [51.70, 1.55],       // north east boundary
+        [50.70, -0.5]        // south west boundary
+    ],
+    maxBoundsViscosity: 0.5, // elastic bounce-back of map edges
 });
 
-/* Add markers for bandstands */
+// Add markers for bandstands 
 
-let markers = [];
+var markers = [];
 
 markers.push({
     markerLat:  51.37268,
@@ -20,7 +28,8 @@ markers.push({
     locationTown:     "Herne Bay",
     locationCounty:   "Kent",
     locationPostCode: "CT6 5JN",
-    favFlag:          false
+    favFlag:          false,
+    _id:              0
 });
 
 markers.push({
@@ -31,7 +40,8 @@ markers.push({
     locationTown:     "Walmer, Deal",
     locationCounty:   "Kent",
     locationPostCode: "CT14 7DX", 
-    favFlag:          false
+    favFlag:          false,
+    _id:              1
 });
 
 markers.push({
@@ -42,14 +52,80 @@ markers.push({
     locationTown:     "Broadstairs",
     locationCounty:   "Kent",
     locationPostCode: "CT10 1QS",
-    favFlag:          false
+    favFlag:          false,
+    _id:              2
 }); 
 
-let markerPopUp = [];
-for (i in markers) {
-    markerPopUp[i] = L.marker([markers[i].markerLat, markers[i].markerLong])
-    .bindPopup(`${markers[i].bandstandName}
-                ${markers[i].locationCounty}
-                ${markers[i].locationPostCode}`)
-    .addTo(mymap);
-};
+// apply listener for clicked markers and apply all markers with popup formatted
+var myFeatureGroup = L.featureGroup().addTo(mymap).on("click", groupClick);
+var marker, test, id;
+
+var markerPopUp  = [];
+var selectedFavs = [];
+markers.forEach(marker=>{
+    id = marker._id;
+    console.log(id);
+    test = id;
+    markerPopUp[id] = L.marker([marker.markerLat, marker.markerLong])
+    .bindPopup(`${id}# ${marker.bandstandName}<br>
+                ${marker.locationCounty}
+                ${marker.locationPostCode}<br>
+                <button onclick="toggleFavFlag(markers)">Toggle as Favourite</button>`)
+    .addTo(myFeatureGroup);
+    markerPopUp[id].test = test;
+    markerPopUp[id]._id = id;
+})
+
+function groupClick(event) {
+    id = event.layer.test;
+    console.log(event.layer);
+}
+
+function toggleFavFlag(markers) {
+    /* id now available from groupClick(event) */
+    
+    for (marker in markers) {
+        if (markers[marker]._id === id) { // clicked marker entry found
+            // set button function in pop up according to favFlag value /
+            if (markers[id].favFlag) {    // currently marked as favourite ... so remove
+
+                // adjust marker 
+                markers[id].favFlag = false;  // set this entry to go through else branch next time through 
+
+                // remove entry from favAreaContent 
+                let removeFavString   = `<p>${markers[id].bandstandName}</p>`;
+
+                let favAreaContent = document.getElementById("fav-area-content");
+                let favWorkArea = favAreaContent.innerHTML;   // grab current favourites
+
+                // the following logic is based on each Bandstand entry being enclosed in <p> </p> pair
+                removeEntryBeg   = favWorkArea.search(removeFavString)             // Find start point of remove entry
+                removeEntryToEnd = favWorkArea.slice(removeEntryBeg)               // Grab from start of remove entry
+                removeEntryEnd   = removeEntryBeg + removeEntryToEnd.search("</p>") + 4;  // ... to determine end point
+                let beforeEntry  = favWorkArea.slice(0,removeEntryBeg);
+                let afterEntry   = favWorkArea.slice(removeEntryEnd);
+                let newFavAreaContent = beforeEntry + afterEntry;
+
+                favAreaContent.innerHTML = newFavAreaContent; 
+
+            } else {                       // currently not marked as favourite ... so add
+
+                console.log("select");
+                
+                // adjust marker                  
+                markers[id].favFlag = true;   // set this entry to go through if branch next time through 
+
+                // format new entry for favAreaContent 
+                let addFavString   = `<p>${markers[id].bandstandName}</p>`;
+
+                // get existing displayed content 
+                let favAreaContent = document.getElementById("fav-area-content");
+
+                // append new entry 
+                favAreaContent.innerHTML = `${favAreaContent.innerHTML}<p>${markers[id].bandstandName}</p>`;
+                
+            }
+        }
+    }
+    return (markers);
+}
