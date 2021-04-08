0f7352489bc1817f1fa9463649e47a5867818005
tubaman48 <rjlovett48@gmail.com>
2021-04-08 04:19:24 +0000

    add outline of confirmSubmitMsg function for receive favourites modal form

--- assets/js/mymodals.js
+++ assets/js/mymodals.js
@@ -1,9 +1,19 @@
+// Receive Favourites modal form functions
+
 function populateSelectedFavs() {
     // grab current favourites from Selected Bandstands area of Main Page
     let favAreaContent = document.getElementById("fav-area-content");
     let favWorkArea = favAreaContent.innerHTML;   
 
-    // pass over to Receive Favourites modal form area
+    // pass over to Receive Favourites modal form area with id #fav-list
     let favList = document.getElementById("fav-list");
     let favList.innerHTML = favWorkArea
+}
+
+function confirmSubmitMsg() {
+    // write out following message in Receive Favourites modal form area with id #completion-msg
+    let completionMsg = document.getElementById("completion-msg");
+    let emailInput    = document.getElementById("contactEmail").value;
+    let favList.innerHTML = `<h3>Details of selected favourites emailed to : ${emailInput}<br>
+                                <button type="button" class="btn btn-success">OK</button></h3>`;
 }
