154f65aa5cacbef7d5f55df1f53b7e0ad8e594a3
tubaman48 <rjlovett48@gmail.com>
2021-06-27 23:34:51 +0000

    add formatBandstandAmendmentsEmail function to mymodals.js

--- assets/js/mymodals.js
+++ assets/js/mymodals.js
@@ -50,6 +50,28 @@ function formatFavsEmail(favsForm) {
 
 }
 
+function formatBandstandAmendmentsEmail(amendForm) {
+
+// invoke emailjs (Service ID = service_blkm6ga and Template ID = bs_amend)
+    emailjs.send("service_blkm6ga","bs_amend",{
+        fname_provider:  amendForm.fname_provider.value,
+        email_provider:  amendForm.email_provider.value,
+        bandstand_name:  amendForm.bandstand_name.value,
+        town_location:   amendForm.town_location.value,
+        county_location: amendForm.county_location.value,
+        post_code:       amendForm.post_code.value,
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+}
+
 function confirmSubmitMsg() {
     // write out following message in Receive Favourites modal form area with id #completion-msg
     let completionMsg = document.getElementById("completion-msg");
